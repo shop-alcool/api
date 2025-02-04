3db9596072ec93debe437a5583785c8126dc6bf5
Th4End <eliazidb@gmail.com>
2025-02-05 00:25:08 +0100

    modification massif + cassage de crâne lié au récupérage des alcool de la base de donnée

--- Donnée/Connexion_DB.js
+++ Donnée/Connexion_DB.js
@@ -1,28 +1,29 @@
 const { Client } = require('pg');
-
-// Créer une instance du client PostgreSQL
+require('dotenv').config({path: './.env'});
+console.log(process.env.USER);
+console.log(process.env.DATABASE);
 const client = new Client({
-  host: 'localhost', // ou l'adresse de ton serveur PostgreSQL
-  port: 5432,        // Port par défaut de PostgreSQL
-  user: 'ton_utilisateur', // Ton nom d'utilisateur
-  password: 'ton_mot_de_passe', // Ton mot de passe
-  database: 'ta_base_de_donnees', // Nom de ta base de données
+  host: process.env.HOST,
+  port: 5432,
+  user: process.env.USER,
+  password: process.env.PWD,
+  database: process.env.DATABASE, 
 });
 
-// Connexion à la base de données
+
+
 client.connect()
   .then(() => {
     console.log('Connecté à la base de données');
-    
-    // Exécuter une requête
-    return client.query('SELECT * FROM ta_table');
   })
   .then(res => {
-    console.log(res.rows); // Afficher les résultats
+    console.log(res.rows);
   })
   .catch(err => {
     console.error('Erreur de connexion:', err);
   })
   .finally(() => {
-    client.end(); // Fermer la connexion
+    client.end();
   });
+
+module.exports = client;
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express.Router();
+const client = require('../Donnée/Connexion_DB');
 
 app.get('/', function (req, res) {
     res.json({ message: 'Hello, world!' });
+});
+
+app.get('/alcool', function (req, res) {
+    client.query('SELECT * FROM alcohol')
+        .then(result => {
+            if (result.rows.length === 0) {
+                console.log(rows);
+                return res.status(404).json({ message: 'Aucun alcool trouvé' });
+            }
+            res.json(result.rows);
+        })
+        .catch(err => {
+            console.log(rows);
+            console.error('Erreur de la requête SQL:', err);
+            res.status(500).json({ message: 'Erreur interne du serveur', error: err });
+        });
 });
 
 module.exports = app;
