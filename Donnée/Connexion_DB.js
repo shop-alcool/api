2ba949c0ae7e81105d738f718a543c069ad40d98
Th4End <eliazidb@gmail.com>
2025-02-04 19:02:33 +0100

    ajout de postgresql

--- Donnée/Connexion_DB.js
+++ Donnée/Connexion_DB.js
@@ -1,4 +1,28 @@
-import { PrismaClient } from '@prisma/client';
-import datasource from './datasource';
+const { Client } = require('pg');
 
-const prisma = new PrismaClient({  datasources: datasource });
+// Créer une instance du client PostgreSQL
+const client = new Client({
+  host: 'localhost', // ou l'adresse de ton serveur PostgreSQL
+  port: 5432,        // Port par défaut de PostgreSQL
+  user: 'ton_utilisateur', // Ton nom d'utilisateur
+  password: 'ton_mot_de_passe', // Ton mot de passe
+  database: 'ta_base_de_donnees', // Nom de ta base de données
+});
+
+// Connexion à la base de données
+client.connect()
+  .then(() => {
+    console.log('Connecté à la base de données');
+    
+    // Exécuter une requête
+    return client.query('SELECT * FROM ta_table');
+  })
+  .then(res => {
+    console.log(res.rows); // Afficher les résultats
+  })
+  .catch(err => {
+    console.error('Erreur de connexion:', err);
+  })
+  .finally(() => {
+    client.end(); // Fermer la connexion
+  });
