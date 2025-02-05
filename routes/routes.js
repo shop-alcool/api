6059867d687404f165bd7363d0d9e45173ab4057
kvnmri <kvn.mri.dev@gmail.com>
2025-02-05 11:56:32 +0100

    ok ok

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,8 @@ const app = express.Router();
 const jwtCheck = require('../Controller/Auth/Auth');
 const client = require('../Donnée/Connexion_DB');
 
+app.use(express.json());
+
 app.get('/', function (req, res) {
     res.send({ message: 'Hello, world!' });
 });
@@ -46,7 +48,7 @@ app.get('/alcool/:id', function (req, res) {
 
 app.post('/alcool', function (req, res) {
     const { type_alcohol_id, name, description, degree, capacity, image } = req.body;
-    client.query('INSERT INTO alcohol(type_alcohol_id, name, description, degree, capacity, image) VALUES($1, $2, $3, $4, $5, $6)', [type_alcohol_id, name, description, degree, capacity, image])
+    client.query('INSERT INTO alcohol (type_alcohol_id, name, description, degree, capacity, image) VALUES ($1, $2, $3, $4, $5, $6)', [type_alcohol_id, name, description, degree, capacity, image])
         .then(result => {
             res.status(201).send(result.rows[0]);
         })
@@ -58,7 +60,7 @@ app.post('/alcool', function (req, res) {
 
 app.patch('/alcool/:id', function (req, res) {
     const { type_alcohol_id, name, description, degree, capacity, image, id } = req.body;
-    client.query('UPDATE alcohol SET type_alcohol_id = $1 name = $2, description = $3, degree = $4, capacity = $5, image = $6 WHERE id = $7 RETURNING *', [type_alcohol_id, name, description, degree, capacity, image, id])
+    client.query('UPDATE alcohol SET type_alcohol_id = $1 name = $2, description = $3, degree = $4, capacity = $5, image = $6 WHERE id = $7', [type_alcohol_id, name, description, degree, capacity, image, id])
         .then(result => {
             if (result.rows.length === 0) {
                 return res.status(404).send({ message: 'Aucun alcool trouvé' });
