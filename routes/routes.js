6e1475a78fb76537d90bb9f236630883518a1bb0
kvnmri <kvn.mri.dev@gmail.com>
2025-02-05 14:45:40 +0100

    update

--- routes/routes.js
+++ routes/routes.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 //     res.send('Secured Resource');
 // });
 
-app.get('/alcool', function (res) {
+app.get('/alcool', function (req, res) {
     client.query('SELECT shop_item.id, alcohol_id, shop_id, price, name, image FROM shop_item INNER JOIN alcohol ON alcohol.id = shop_item.alcohol_id;')
         .then(result => {
             if (result.rows.length === 0) {
@@ -60,7 +60,7 @@ app.post('/alcool', function (req, res) {
 
 app.patch('/alcool/:id', function (req, res) {
     const { type_alcohol_id, name, description, degree, capacity, image, id } = req.body;
-    client.query('UPDATE alcohol SET type_alcohol_id = $1 name = $2, description = $3, degree = $4, capacity = $5, image = $6 WHERE id = $7', [type_alcohol_id, name, description, degree, capacity, image, id])
+    client.query('UPDATE alcohol SET type_alcohol_id = $1, name = $2, description = $3, degree = $4, capacity = $5, image = $6 WHERE id = $7', [type_alcohol_id, name, description, degree, capacity, image, id])
         .then(result => {
             if (result.rows.length === 0) {
                 return res.status(404).send({ message: 'Aucun alcool trouvé' });
