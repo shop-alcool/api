0a17d46a3dd7888bda587bf0f2b6139389363041
Th4End <eliazidb@gmail.com>
2025-02-05 11:12:33 +0100

    ok know

--- routes/routes.js
+++ routes/routes.js
@@ -45,8 +45,8 @@ app.get('/alcool/:id', function (req, res) {
 });
 
 app.post('/alcool', function (req, res) {
-    const { type_alcohol_id, name, price, quantity, description } = req.body;
-    client.query('INSERT INTO alcohol(type_alcohol_id, name, price, quantity, description) VALUES($1, $2, $3, $4, $5) RETURNING *', [type_alcohol_id, name, price, quantity, description])
+    const { type_alcohol_id, name, description, degree, capacity, image } = req.body;
+    client.query('INSERT INTO alcohol(type_alcohol_id, name, description, degree, capacity, image) VALUES($1, $2, $3, $4, $5, $6) RETURNING *', [type_alcohol_id, name, description, degree, capacity, image])
         .then(result => {
             res.status(201).send(result.rows[0]);
         })
@@ -57,8 +57,8 @@ app.post('/alcool', function (req, res) {
 });
 
 app.patch('/alcool/:id', function (req, res) {
-    const { type_alcohol_id, name, price, quantity, description, id } = req.body;
-    client.query('UPDATE alcohol SET type_alcohol_id = $1 name = $2, price = $3, quantity = $4, description = $5 WHERE id = $6 RETURNING *', [type_alcohol_id, name, price, quantity, description, id])
+    const { type_alcohol_id, name, description, degree, capacity, image, id } = req.body;
+    client.query('UPDATE alcohol SET type_alcohol_id = $1 name = $2, description = $3, degree = $4, capacity = $5, image = $6 WHERE id = $7 RETURNING *', [type_alcohol_id, name, description, degree, capacity, image, id])
         .then(result => {
             if (result.rows.length === 0) {
                 return res.status(404).send({ message: 'Aucun alcool trouvé' });
